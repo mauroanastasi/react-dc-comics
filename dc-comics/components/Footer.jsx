07b2af056b85239085800de69e81bba8ca40fa8a
mauroanastasi <mauroanastasi1@gmail.com>
2025-05-06 09:15:17 +0200

    [UPD] img e descr footer up

--- dc-comics/components/Footer.jsx
+++ dc-comics/components/Footer.jsx
@@ -2,7 +2,26 @@ const Footer = () => {
     return (
         <>
             <div className="contentFooterUp">
-
+                <div>
+                    <img src="/buy-comics-digital-comics.png" alt="" />
+                    <span>digital comics</span>
+                </div>
+                <div>
+                    <img src="/buy-comics-merchandise.png" alt="" />
+                    <span>dc merchandise</span>
+                </div>
+                <div>
+                    <img src="/buy-comics-shop-locator.png" alt="" />
+                    <span>subscription</span>
+                </div>
+                <div>
+                    <img src="/buy-comics-subscriptions.png" alt="" />
+                    <span>comic shop locator</span>
+                </div>
+                <div>
+                    <img src="/buy-dc-power-visa.svg" alt="cartaCredito" />
+                    <span>dc powervisa</span>
+                </div>
             </div>
             <div className="contentFooterCentral">
 
