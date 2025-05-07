b20d8115ed3dca2f10d62bf9e5661c4ec3e003f5
mauroanastasi <mauroanastasi1@gmail.com>
2025-05-07 09:15:25 +0200

    [UPD] creo la prima card in ListComics

--- dc-comics/components/ListComics.jsx
+++ dc-comics/components/ListComics.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
 const ListComics = () => {
   return (
-    
+    <section>
+      <div className='container card-container'>
+        <div className='comics-card'>
+          <img src="https://imgs.search.brave.com/aUNyvZBXUulb963JH7KnQm9AMr8bcBoLsiHREOqayIU/rs:fit:612:612:1/g:ce/aHR0cHM6Ly9pNS53/YWxtYXJ0aW1hZ2Vz/LmNvbS9hc3IvOWZm/ZWYzMDMtMGZhYy00/OGRkLTg3ODctYzUy/NTk0MDk2ODAwXzEu/MTc1ZDk1Mjg2NzY0/OGEwOTczMTY2NGMy/MTE1NjNlYWIuanBl/Zz9vZG5XaWR0aD02/MTImb2RuSGVpZ2h0/PTYxMiZvZG5CZz1m/ZmZmZmY" alt="Deluxeed" />
+          <h3>Action Comics #1000: The Deluxe Edition</h3>
+        </div>
+      </div>
+    </section>
   );
 }
 
